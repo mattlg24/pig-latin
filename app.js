ff226cf34d36484483ebe137f2dd6dec0c54edeb
Matt Gardner <matthewlgardner@gmail.com>
2018-09-05 17:00:03 -0600

    can grab user text in JS

--- app.js
+++ app.js
@@ -3,6 +3,25 @@ var myApp = angular.module('pigLatinApp', [])
 
 myApp.controller('mainController', ['$scope', function($scope) {
 
+    // grab text from user
+    $scope.userText = ''
+    var vowel = ['a', 'e', 'i', 'o', 'u']
+    $scope.clicked = function() {
+        var text = $scope.userText
+
+        // regex to rework the order to match pig Latin
+        for (var i = 0; i < text.length; i++) {
+            // console.log(text[i])
+            if (text[i] != 'a' || text[i] != 'e' || text[i] != 'i' || text[i] != 'o' || text[i] != 'u') {
+                console.log(text.slice(1) + '-' + text[i])
+            }
+        }
+
+
+
+        //set as scope variable
+
+    }
 
 
 }])
