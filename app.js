765e05c51a9d308bc4f4168ee66059945b504816
Matt Gardner <matthewlgardner@gmail.com>
2018-09-05 22:31:09 -0600

    added logic to make text pig latin

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
 // maybe connect to a dictionary to get word meanings
 var myApp = angular.module('pigLatinApp', [])
 
-myApp.controller('mainController', ['$scope', function($scope) {
+myApp.controller('mainController', [
+    '$scope',
+    function($scope) {
 
-    // grab text from user
-    $scope.userText = ''
-    var vowel = ['a', 'e', 'i', 'o', 'u']
-    $scope.clicked = function() {
-        var text = $scope.userText
+        // grab text from user
+        $scope.userText = ''
+        $scope.clicked = function() {
+            var text = $scope.userText
+console.log('text ', text);
 
-        // regex to rework the order to match pig Latin
-        for (var i = 0; i < text.length; i++) {
-            // console.log(text[i])
-            if (text[i] != 'a' || text[i] != 'e' || text[i] != 'i' || text[i] != 'o' || text[i] != 'u') {
-                console.log(text.slice(1) + '-' + text[i])
+
+            var textArr = $scope.userText.split(' ')
+            var pigArr = []
+            // loop through textArr to work on individual word
+            for (var i = 0; i < textArr.length; i++) {
+                // regex to rework the order to match pig Latin
+                var begin = textArr[i].split(/([aeiouy].*)/)[0]
+                var end = textArr[i].split(/([aeiouy].*)/)[1]
+                // check if first letter is a 'y'
+                if (textArr[i].indexOf('y') === 0) {
+                    var begin = textArr[i].split(/([aeiou].*)/)[0]
+                    var end = textArr[i].split(/([aeiou].*)/)[1]
+                }
+                var pigWord = end + '-' + begin + 'ay'
+                pigArr.push(pigWord)
             }
+
+            //set to scope
+            $scope.pigText = pigArr.join(' ')
+            console.log($scope.pigText);
         }
 
-
-
-        //set as scope variable
-
     }
-
-
-}])
+])
