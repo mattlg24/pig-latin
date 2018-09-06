f08e3f7b19fe793b07d3fbd5462fd558ae013e52
Matt Gardner <matthewlgardner@gmail.com>
2018-09-05 23:01:05 -0600

    validate to exclude numbers

--- app.js
+++ app.js
@@ -8,29 +8,33 @@ myApp.controller('mainController', [
         // grab text from user
         $scope.userText = ''
         $scope.clicked = function() {
+            //check if text does not contain numbers
             var text = $scope.userText
-console.log('text ', text);
-
-
-            var textArr = $scope.userText.split(' ')
-            var pigArr = []
-            // loop through textArr to work on individual word
-            for (var i = 0; i < textArr.length; i++) {
-                // regex to rework the order to match pig Latin
-                var begin = textArr[i].split(/([aeiouy].*)/)[0]
-                var end = textArr[i].split(/([aeiouy].*)/)[1]
-                // check if first letter is a 'y'
-                if (textArr[i].indexOf('y') === 0) {
-                    var begin = textArr[i].split(/([aeiou].*)/)[0]
-                    var end = textArr[i].split(/([aeiou].*)/)[1]
+            var alpha = /^[^0-9]+$/
+            if (text.match(alpha)) {
+                $scope.pigText = ''
+                var textArr = $scope.userText.split(' ')
+                var pigArr = []
+                // loop through textArr to work on individual word
+                for (var i = 0; i < textArr.length; i++) {
+                    // regex to rework the order to match pig Latin
+                    var begin = textArr[i].split(/([aeiouy].*)/)[0]
+                    var end = textArr[i].split(/([aeiouy].*)/)[1]
+                    // check if first letter is a 'y'. if yes, include 'y' with begin var
+                    if (textArr[i].indexOf('y') === 0) {
+                        var begin = textArr[i].split(/([aeiou].*)/)[0]
+                        var end = textArr[i].split(/([aeiou].*)/)[1]
+                    }
+                    var pigWord = end + '-' + begin + 'ay'
+                    pigArr.push(pigWord)
                 }
-                var pigWord = end + '-' + begin + 'ay'
-                pigArr.push(pigWord)
+                //set to scope
+                $scope.pigText = pigArr.join(' ')
+                $scope.errorMsg = ''
+            } else {
+                $scope.errorMsg = 'Enter letters only.'
             }
 
-            //set to scope
-            $scope.pigText = pigArr.join(' ')
-            console.log($scope.pigText);
         }
 
     }
