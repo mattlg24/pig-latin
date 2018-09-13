35400b22d526ef9376b992980264e93cb904f223
Matt Gardner <matthewlgardner@gmail.com>
2018-09-13 17:01:10 -0600

    add letter scramble

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 // be able to tweet your pig latin text (maybe make it a character limit for twitter)
 // strip out and capture punctuation and append to end of text
 // ng model on usertext and then css effects to jumble letters into pig latin https://codepen.io/guardian/pen/EjeXOZ
+// error if no vowels
 var myApp = angular.module('pigLatinApp', [])
 
 myApp.controller('mainController', [
@@ -13,6 +14,10 @@ myApp.controller('mainController', [
         // grab text from user
         $scope.userText = ''
         $scope.clicked = function() {
+
+            // clear .random element each time button clicked
+            $('.random').empty()
+
             var text = $scope.userText
             // check for punctuation and strip out
             // var strip = /[\?.\!]+/
@@ -58,7 +63,52 @@ myApp.controller('mainController', [
                 $scope.errorMsg = 'Enter letters only'
             }
 
-        }
+            // split userText into an array of individual letters
+            var letters = text.split('')
+            var timer = 50
+            var data = 0
 
-    }
+            // loop through and create <span> for each letter and add data-change attribute
+            for (var i = 0; i < letters.length; i++) {
+                var change = Math.round(Math.random() * 100)
+                var el = `<span class="nbr ltr" data-change="${change}">${letters[i]}</span>`
+                $('.random').append(el)
+            }
+
+            var randomnbr = $('.nbr')
+            // console.log('randomnbr ', randomnbr);
+            // var change
+            // for (var i = 0; i < randomnbr.length; i++) {
+            //     change = Math.round(Math.random() * 100)
+            //     $(randomnbr[i]).attr('data-change', change)
+            // }
+
+            function random() {
+                return Math.round(Math.random() * 9)
+            }
+
+            function select() {
+                return Math.round(Math.random() * randomnbr.length + 1)
+            }
+
+
+            function value() {
+                console.log($(`.nbr:nth-child(${select()})`).html(random()));
+                console.log($(`.nbr:nth-child(${select()})`).attr('data-number', data));
+                data++
+
+                for (var i = 0; i < randomnbr.length; i++) {
+                    if (parseInt($(randomnbr[i]).attr('data-number')) > parseInt($(randomnbr[i]).attr('data-change'))) {
+                        var index = $('.ltr').index(randomnbr[i])
+                        $(randomnbr[i]).html(letters[index])
+                        $(randomnbr[i]).removeClass('nbr')
+                    }
+                }
+            }
+
+            setInterval(value, timer)
+
+        } // end of click function
+
+    } // end of controller
 ])
